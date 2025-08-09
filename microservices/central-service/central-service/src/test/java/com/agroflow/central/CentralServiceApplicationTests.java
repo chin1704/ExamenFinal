cbc2ff41d51c8ab55e7b41596dea92cda035743a
chin1704 <109291624+chin1704@users.noreply.github.com>
2025-08-08 17:56:01 -0700

    inicio
    commit inicial

---
+++ microservices/central-service/central-service/src/test/java/com/agroflow/central/CentralServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroflow.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
